5208f85bda3b727341ab20b9a242572b0b0364d1
weichangk <18276743761@163.com>
2022-10-18 00:12:42 +0800

    add

--- VideoCompression/ViewModels/FileManageViewModel.cs
+++ VideoCompression/ViewModels/FileManageViewModel.cs
@@ -1,5 +1,10 @@
-﻿using Prism.Mvvm;
+﻿using FFMpegNetWrapper;
+using Microsoft.Win32;
+using Prism.Commands;
+using Prism.Mvvm;
 using Prism.Regions;
+using System;
+using System.Threading.Tasks;
 using VideoCompression.Views;
 
 namespace VideoCompression.ViewModels;
@@ -8,15 +13,39 @@ public class FileManageViewModel : BindableBase
 {
     readonly IRegionManager regionManager;
 
+    public DelegateCommand OpenFileCommand { get; private set; }
+
     public FileManageViewModel(IRegionManager regionManager)
     {
         this.regionManager = regionManager;
 
         Configure();
+        //var result = Instances.Instance.Finish(@"./FFMPEG/bin/ffprobe.exe", "-version");
     }
 
     private void Configure()
     {
-        regionManager.RegisterViewWithRegion("FileListRegion", typeof(FileListView));
+        //regionManager.RegisterViewWithRegion("FileListRegion", typeof(FileListView));
+        OpenFileCommand = new DelegateCommand(OpenFile);
+    }
+
+    private void OpenFile()
+    {
+        OpenFileDialog dlg = new()
+        {
+            DefaultExt = ".txt",
+            Filter = "All Files|*.*"
+        };
+        bool result = (bool)dlg.ShowDialog();
+        if (result == true)
+        {
+            string filename = dlg.FileName;
+            LoadFileAsync(filename);
+        }
+    }
+
+    private void LoadFileAsync(string filename)
+    {
+        var fileAnalysis =  FFProbe.Analyse(filename);
     }
 }
